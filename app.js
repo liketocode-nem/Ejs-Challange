9d104228ee09198d450401eef871567f0e9a36c5
Nathan Miller <davidhmiller00@hotmail.com>
2023-02-10 20:12:23 -0500

    add all of the pages

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -15,18 +13,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get('/', (req, res) => {
+  res.render("home",{home:homeStartingContent});
+});
 
+app.get('/about', (req, res) => {
+  res.render("about",{about:aboutContent});
+});
 
-
-
-
-
-
-
-
-
-
-
+app.get('/contact', (req, res) => {
+  res.render("contact",{contact:contactContent});
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
